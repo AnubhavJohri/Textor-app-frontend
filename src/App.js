490d713d9a05e1b00b6fa1d1fcad4f2958858778
anubhavjohri <anubhavj22@gmail.com>
2019-12-06 17:14:11 +0530

    Added History Component and Navbar Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch ,Redirect} from 'react-router-dom';
 import './App.css';
 import Home from "./components/Home";
+import Navbar1 from "./components/Navbar";
+import History from "./components/History" ;
 
 
 
@@ -12,8 +14,11 @@ class App extends Component {
       <div>
         <Router>
           <Fragment>
+             <Navbar1/>
             <Switch>
               <Route exact path='/home' component={Home} />
+              <Route exact path='/navbar' component={Navbar1} />
+              <Route exact path='/history' component={History} />
               <Route exact path='/**' render={()=><Redirect to="/home"/>} />
             </Switch>
           </Fragment>
---
+++ src/components/History.js
@@ -0,0 +1,16 @@
+import React from 'react' ;
+export default class History extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <div className = "container">
+                    <div className = "row">
+                        <div className = "col-lg-8 offset-lg-2">
+                            <h1 className = "display-2">Coming Soon!</h1>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
