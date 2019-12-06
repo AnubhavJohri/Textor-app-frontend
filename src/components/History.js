f4e2a9df070ab0f8619858434f6d0c6ae7656338
anubhavjohri <anubhavj22@gmail.com>
2019-12-06 17:21:14 +0530

    Few Tweeks

--- src/components/History.js
+++ src/components/History.js
@@ -1,8 +1,10 @@
 import React from 'react' ;
+import DocumentTitle from 'react-document-title';
 export default class History extends React.Component {
     render () {
         return (
             <React.Fragment>
+                <DocumentTitle title = "Textor | History"/>
                 <div className = "container">
                     <div className = "row">
                         <div className = "col-lg-8 offset-lg-2">
